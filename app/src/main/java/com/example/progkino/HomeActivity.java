d86306a87656dad56be8402a5f0c011c1fb30067
Pavel505 <pavlik.pichugin.2002@mail.ru>
2024-01-15 01:35:30 +0300

    Доделан адаптер. Фильтры свайпаются, цвет?

--- app/src/main/java/com/example/progkino/HomeActivity.java
+++ app/src/main/java/com/example/progkino/HomeActivity.java
@@ -1,14 +1,43 @@
 package com.example.progkino;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+import com.example.progkino.Models.Category;
+import com.example.progkino.adapter.CategoryAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class HomeActivity extends AppCompatActivity {
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1,"Интеллект"));
+        categoryList.add(new Category(2,"Настолки"));
+        categoryList.add(new Category(3,"Сюжетки"));
+        categoryList.add(new Category(4,"Турниры"));
+
+        setCategoryRecycler(categoryList);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+        // Эдементы идут горизонтально
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
--- app/src/main/java/com/example/progkino/adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/progkino/adapter/CategoryAdapter.java
@@ -1,13 +1,17 @@
 package com.example.progkino.adapter;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.progkino.Models.Category;
+import com.example.progkino.R;
 
 import java.util.List;
 
@@ -24,12 +28,15 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     @NonNull
     @Override
     public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // Использование определенного дизайна через описание вложенного класса CategoryViewHolder
+        View categoryItems = LayoutInflater.from(context).inflate(R.layout.category_item, parent , false);
+        return new CategoryViewHolder(categoryItems);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
-
+// Что мы будем подставлять в конкретное поле
+        holder.categoryTitle.setText(categories.get(position).getTitle());
     }
 
     @Override
@@ -38,9 +45,10 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     }
 
     public static final class CategoryViewHolder extends RecyclerView.ViewHolder{
-
+        TextView categoryTitle;
         public CategoryViewHolder(@NonNull View itemView) {
             super(itemView);
+            categoryTitle = itemView.findViewById(R.id.categoryTitle);
         }
     }
 }
