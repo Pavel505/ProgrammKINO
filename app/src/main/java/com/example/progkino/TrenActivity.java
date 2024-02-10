a8d59bc3c6d2580d71bf681f79c1a0ff5de95f87
Pavel505 <pavlik.pichugin.2002@mail.ru>
2024-02-10 16:07:24 +0300

    Заготовки для таймера

--- app/src/main/java/com/example/progkino/TrenActivity.java
+++ app/src/main/java/com/example/progkino/TrenActivity.java
@@ -2,15 +2,31 @@ package com.example.progkino;
 
 import static android.content.ContentValues.TAG;
 
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
 import static com.example.progkino.HomeActivity.eventumList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TrenActivity extends AppCompatActivity {
 
@@ -49,9 +65,86 @@ public class TrenActivity extends AppCompatActivity {
         startActivity(intentTrening2);
     }
 
+    public void onClickRead3 (View view) throws IOException {
+        /*FileInputStream fin = null;
+       // TextView textView = findViewById(R.id.text);
+        try {
+            fin = openFileInput("skazki.md");
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String (bytes);
+            //textView.setText(text);
+        }
+        catch(IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        finally{
+            try{
+                if(fin!=null)
+                    fin.close();
+            }
+            catch(IOException ex){
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }*/
+        /*try {
+            // открываем поток для чтения
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    openFileInput(("skazki.md"))));
+            String str = "";
+            // читаем содержимое
+            while ((str = br.readLine()) != null) {
+
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        /*Intent intentTrening3 = new Intent(TrenActivity.this, Trening2Activity.class);
+        intentTrening3.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        startActivity(intentTrening3);*/
+        /*File file = new File("C:\\Users\\root\\Desktop\\ProgKino\\app\\src\\main\\assets\\skazki.txt");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        int data = fileInputStream.read();
+        while (data != -1) {
+// Обработка данных из файла
+            data = fileInputStream.read();
+        }
+        fileInputStream.close();*/
+    }
+
     /*
     public void navigatorTrening(View view){
+public void openText(View view){
 
+        FileInputStream fin = null;
+        TextView textView = findViewById(R.id.text);
+        try {
+            fin = openFileInput(FILE_NAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String (bytes);
+            textView.setText(text);
+        }
+        catch(IOException ex) {
+
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        finally{
+
+            try{
+                if(fin!=null)
+                    fin.close();
+            }
+            catch(IOException ex){
+
+                Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     }*/
 
 
@@ -69,25 +162,14 @@ public class TrenActivity extends AppCompatActivity {
         return s;
     }*/
 /*
-    public String f(){
-        String filename = "1geografy.docx";
-        String text = filename;
-        byte[] buffer = null;
-        InputStream is;
-        try {
-            is = getAssets().open(text);
-            int size = is.available();
-            buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String str_data = new String(buffer);
-        return str_data;
+    public String f(View view){
+        String filename = "skazki.txt";
+
     }*/
+
 /*
-    public Bitmap loadBitmapFromAssets(Context context, String filename) {
+    public Bitmap loadBitmapFromAssets(Context context) {
+        String filename = "skazki.txt";
         AssetManager assetManager = context.getAssets();
         try {
             InputStream is = assetManager.open(filename);
@@ -99,5 +181,4 @@ public class TrenActivity extends AppCompatActivity {
             return BitmapFactory.decodeStream(null,null,null);
         }
     }*/
-
 }
--- app/src/main/java/com/example/progkino/TreningActivity.java
+++ app/src/main/java/com/example/progkino/TreningActivity.java
@@ -22,19 +22,23 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TreningActivity extends AppCompatActivity {
     private ListView listView;
     String answer;
     private ArrayAdapter<String> adapterAr;
     private List<String> listData;
-    public int id_q, counter,counter_question ;
-    TextView text_question_country;
+    public int id_q, counter,counter_question, ost_time ;
+    TextView text_question_country,text_time;
     DatabaseReference  treningGeo1;
     EditText editTextAnswer;
     FirebaseDatabase db;
+    Timer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +48,7 @@ public class TreningActivity extends AppCompatActivity {
     }
 
     private void init(){
+        text_time = findViewById(R.id.text_time);
         editTextAnswer = findViewById(R.id.answer_capital);
         listView = findViewById(R.id.listCountry);
         text_question_country = findViewById(R.id.text_question_country);
@@ -55,6 +60,7 @@ public class TreningActivity extends AppCompatActivity {
         counter = 0;counter_question = 0;id_q=1;
     }
     private void getDataFromDB(){
+        ost_time = 5;
         ValueEventListener vlistener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -76,14 +82,37 @@ public class TreningActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError error) {}
         };
         treningGeo1.addValueEventListener(vlistener);
+        //tableTime();
     }
 
+    /*public void tableTime(){
+
+        TimerTask task = new TimerTask() {
+            public void run() {
+                Log.w(TAG, "Осталось времени: " + ost_time);
+                ost_time -= 0.5;
+                Log.w(TAG, "Осталось времени: "+ost_time);
+                if (ost_time <= 0){
+                   // timer.cancel();// &?
+                    text_time.setText("Время вышло");
+                }
+                text_time.setText(ost_time);
+            }
+        };
+        Timer timer = new Timer("Timer");
+        long delay = 10000L;
+        long period = 1000L;
+        timer.scheduleAtFixedRate(task, delay, period);
+    }*/
     public void onAnswerOk (View view){
         counter_question  += 1;
         String answer_znatok = editTextAnswer.getText().toString();
         if (answer.equalsIgnoreCase(answer_znatok)){
             counter += 1;
             Log.w(TAG, "Правильный ответ " + counter + "/" + counter_question );
+        }
+        if (ost_time<0){
+            counter -= 0.5;
         }
         Log.w(TAG, "Другой ответ " + id_q + " " + answer + " " + answer_znatok);
         getDataFromDB();
