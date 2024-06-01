4bd69f355f4bc4baaf47277d77762077aafe89f8
Pavel505 <pavlik.pichugin.2002@mail.ru>
2024-06-01 15:57:17 +0300

    Форма для предупреждений от админа

--- app/src/main/java/com/example/progkino/ReviewShowActivity_Admin.java
+++ app/src/main/java/com/example/progkino/ReviewShowActivity_Admin.java
@@ -8,24 +8,32 @@ import static com.example.progkino.Constant.REVIEW_CONTENT;
 import static com.example.progkino.Constant.REVIEW_TEMA;
 import static com.example.progkino.Constant.REVIEW_TIME;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.picasso.Picasso;
 
 public class ReviewShowActivity_Admin extends AppCompatActivity {
     private TextView text_author, text_time,text_tema,text_review;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review_show_admin);
         init();
         getIntentMain_Review();
+
     }
 
     private void init(){
@@ -35,9 +43,10 @@ public class ReviewShowActivity_Admin extends AppCompatActivity {
         text_review = findViewById(R.id.textView31_otzv);
     }
 
-    private void getIntentMain_Review(){
+    public void getIntentMain_Review(){
         Intent i2 = getIntent();
         if(i2 != null){
+
             text_author.setText(i2.getStringExtra(REVIEW_AUTHOR));
             text_time.setText(i2.getStringExtra(REVIEW_TIME));
             text_tema.setText(i2.getStringExtra(REVIEW_TEMA));
@@ -48,5 +57,32 @@ public class ReviewShowActivity_Admin extends AppCompatActivity {
         Intent intentTren = new Intent(this, HomeActivity_Admin.class);
         intentTren.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intentTren);
+    }
+
+    public void predActivity_Review(View view){
+        AlertDialog.Builder dialog_rev = new AlertDialog.Builder(this);
+        dialog_rev.setTitle("Дать предупреждение пользователю" + " ?");
+        // Создание объекта для нужного шаблона, помещаев его в View
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View review_window = inflater.inflate(R.layout.review_window_admin, null);
+        dialog_rev.setView(review_window);
+        Intent intentTren43 = new Intent(this, HomeActivity_Admin.class);
+
+        dialog_rev.setNegativeButton("Вернуться назад", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog_rev.setPositiveButton("Дать предупреждение", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+
+
+                intentTren43.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intentTren43);
+            }
+        });
+        dialog_rev.show();
     }
 }
